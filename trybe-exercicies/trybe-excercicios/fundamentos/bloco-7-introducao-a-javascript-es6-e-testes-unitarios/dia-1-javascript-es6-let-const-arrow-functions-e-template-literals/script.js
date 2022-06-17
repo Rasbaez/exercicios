696786f7d529d132161b34ab35be940abe19d2e2
Roberto Baez <rbaezsp@gmail.com>
2022-06-17 16:00:40 -0300

    finalizando exercicios template literals

--- trybe-exercicies/trybe-excercicios/fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
+++ trybe-exercicies/trybe-excercicios/fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -49,16 +49,97 @@ arrOrdenator(oddsAndEvens)
 // Na matemática, o fatorial de um número não negativo N, com a notação N!, é o produto de todos os inteiros menores ou iguais a N. Exemplo: 4! = 4 * 3 * 2 * 1 = 24.
 
 
-const calcFactorial = (number) => number === 0 || number === 1 ? 1 : number  * calcFactorial(number -1)
+const calcFactorial = (number) => number === 0 || number === 1 ? 1 : number * calcFactorial(number - 1)
 
 // console.log(calcFactorial(4));
 
 
 //===============================================================================================================
 
+// Crie uma função que receba uma frase e retorne a maior palavra.
+// Exemplo:
+// Copiar
+//       longestWord('Antônio foi no banheiro e não sabemos o que aconteceu') // retorna 'aconteceu'
+
+
+const frase = 'Criando uma função para encontrar a maior palavra nesta frase, adicionando algumas palavras a mais somente para testes!'
+
+const findBiggestWord = (str) => {
+  let biggest = 0;
+  let word = null;
+  const arrOfStrings = str.split(' ');
+
+  arrOfStrings.forEach(str => {
+    if (biggest < str.length) {
+      biggest = str.length;
+      word = str;
+    }
+
+  })
+  return word;
+};
+
+console.log(findBiggestWord(frase))
+
+//==========================================================================================
+// 🚀 Crie uma página com um contador de cliques
+// Sua página deve conter:
+// Um botão ao qual será associado um event listener;
+// Uma variável clickCount no arquivo JavaScript que acumule o número de clicks no botão;
+// Um campo no HTML que vá atualizando a quantidade de clicks no botão conforme a variável clickCount é atualizada.
 
 
 
 
+// const body = document.querySelector('body');
+// const showClicks = document.createElement('p');
+// body.appendChild(showClicks);
+// let clickCount = 0;
+// const btn = document.querySelector('button');
 
 
+// btn.addEventListener('click', () => {
+
+//   clickCount+=1
+//  clickCount === 1 ? showClicks.innerHTML = `Fui clicado: ${clickCount} vez!`
+//                  :  showClicks.innerHTML = `Fui clicado: ${clickCount} vezes!` 
+// });
+
+//==========================================================================================
+// 🚀 Crie duas funções JavaScript com as seguintes especificações:
+// Não se esqueça de usar template literals
+// Função 1: Escreva uma função que substitua a letra 'x' em uma frase.
+// A função deverá receber um nome por parâmetro;
+// Declare dentro da função uma variável com o nome frase, do tipo const e com o valor igual a 'Tryber x aqui!';
+// A função deverá retornar uma nova frase onde o x da frase 'Tryber x aqui!' seja substituído pelo nome passado por parâmetro.
+// Exemplo:
+// Parâmetro: 'Bebeto'
+// Retorno: 'Tryber Bebeto aqui!'
+// Função 2: Escreva uma função que irá receber o retorno da Função 1 por parâmetro e retornará uma nova string.
+// A função deverá receber o retorno da Função 1 por parâmetro;
+// Declare dentro da função uma variável com o nome skills, do tipo const;
+// A variável skills deverá ser um array contendo três strings com tecnologias que você já aprendeu.
+// Concatene o valor retornado da Função 1, a frase 'Minhas três principais habilidades são:' e o valor da variável skills.
+// Exemplo de retorno:
+// Tryber Bebeto aqui!
+// Minhas três principais habilidades são:
+// JavaScript
+// HTML
+// CSS
+
+const removeX = (name) => {
+  const frase = 'Tryber x aqui!';
+  const replaceX = frase.replace('x', name);
+  return replaceX;
+};
+
+removeX('Roberto');
+
+  const habilities = () => {
+   const skills = ['html', 'css', 'JavaScript'];
+
+  return `${removeX('Roberto')}!! Minhas três principais habilidades são: ${skills.join(', ')}`
+
+};
+
+console.log(habilities())
